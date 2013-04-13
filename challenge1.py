a0814660248ca393d93365ace397e71758682f67
rackeric <utcodemonkey@hotmail.com>
2013-04-13 02:48:51 +0000

    new file

---
+++ challenge1.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+#
+# Challenge 1
+#
+# Write a script that builds three 512 MB Cloud Servers
+# that following a similar naming convention. (ie., web1, web2, web3)
+# and returns the IP and login credentials for each server.
+# Use any image you want. Worth 1 point
+#
+import pyrax
+import time
+
+username = "utcodemonkey"
+API_key = "25136ca5cbe07aa925da1ee2f232aa5a"
+
+pyrax.set_credentials(username, API_key)
+
+# create objects
+cs  = pyrax.cloudservers
+
+# define image and flavor
+ubu_image = "5cebb13a-f783-4f8c-8058-c4182c724ccd"
+flavor_512 = 2
+
+# server creation loop
+for i in range(1, 4):
+	#name = 'web' + str(i)
+	#print name
+	server = cs.servers.create('web' + str(i), ubu_image, flavor_512)
+	pyrax.utils.wait_until(server,
+			att = "status", \
+			desired = "ACTIVE", \
+			callback = None, \
+			interval = 15, \
+			attempts = 25, \
+			verbose = True, \
+			verbose_atts = None)
+	print "Admin password:", server.adminPass
+	print "Networks:", server.networks	
