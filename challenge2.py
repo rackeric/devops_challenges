bead725656528a38426937e8e45d0535e505ad16
rackeric <utcodemonkey@hotmail.com>
2013-04-25 18:48:40 +0000

    adding challenges, multiple

--- challenge2.py
+++ challenge2.py
@@ -17,31 +17,28 @@ cs = pyrax.cloudservers
 # define image and flavor
 flavor_512 = 2
 id_of_server = "a997bb89-c33a-4571-81a9-8f676258941a"
-#id_of_server = [img for img in cs.images.list()
-#        if "Ubuntu4nova" in img.name][0]
-
 
 print "1"
 server = cs.servers.get(id_of_server)
 print "2"
-new_img = server.create_image("deleteme7")
+new_img = server.create_image("deleteme13")
 print "3"
 print new_img
 print "-Waiting for image creation."
-pyrax.utils.wait_until(new_img,
-			att = "status", \
-			desired = "available", \
-			callback = None, \
-			interval = 30, \
-			attempts = 25, \
-			verbose = True, \
-			verbose_atts = None)
+img = pyrax.cloudservers.images.get(new_img)
+while img.status != "ACTIVE":
+	time.sleep(60)
+	img = pyrax.cloudservers.images.get(new_img)
+	print "...still waiting"
+
+print "-Image created."
+
 
 # create server based on new image
 newserver = cs.servers.create("challenge2_server", new_img, flavor_512)
 
 # server info print out
-print "Admin password:", server.adminPass
-while not (server.networks):
-	server = cs.servers.get(server.id)
-print "Networks:", server.networks
+print "Admin password:", newserver.adminPass
+while not (newserver.networks):
+	newserver = cs.servers.get(server.id)
+print "Networks:", newserver.networks
